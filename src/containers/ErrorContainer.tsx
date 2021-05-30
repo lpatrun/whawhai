88e227202a16b630c8c3d03b68a43c3daabb9d8b
lpatrun <luka.patrun@gmail.com>
2021-05-30 16:59:46 +0200

    router redirection

--- src/containers/ErrorContainer.tsx
+++ src/containers/ErrorContainer.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useHistory } from 'react-router';
 
 import { useErrorContext } from "../custom-hooks/useErrorContext";
 import { clearError } from "../actions/errorActions";
@@ -7,9 +8,11 @@ import ErrorScreen from '../screens/ErrorScreen';
 
 export default function ErrorContainer() {
   const { errorState, errorDispatch } = useErrorContext();
+  let history = useHistory()
 
   function resetError() {
     errorDispatch(clearError())
+    history.push("/")
   }
 
   return (
