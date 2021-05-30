aa28692ce04c66cbf5eaec8db37aa7782830ab1b
lpatrun <luka.patrun@gmail.com>
2021-05-30 17:01:01 +0200

    new property

--- src/reducer/errorReducer.ts
+++ src/reducer/errorReducer.ts
@@ -4,10 +4,10 @@ import { ErrorActions, ActionType } from "../actions/errorActions";
 export function errorReducer(errorState: ErrorState, action: ErrorActions): ErrorState {
   switch (action.type) {
     case ActionType.SetError:
-      return {...errorState, message: action.payload.message, code: action.payload.code}
+      return {...errorState, error: true, message: action.payload.message, code: action.payload.code, reason: action.payload.reason}
     
     case ActionType.ClearError:
-      return {...errorState, message: "", code: 0}
+      return {...errorState, error: false, message: "", code: 0, reason: undefined}
 
     default:
       return errorState;
